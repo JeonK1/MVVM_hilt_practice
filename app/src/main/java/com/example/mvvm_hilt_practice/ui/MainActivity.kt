e9007d186370d68b2f05cbe5a893c6f4cf2ec1da
JeonK1 <kan5016@naver.com>
2021-08-11 00:07:25 +0900

    write until #3

--- app/src/main/java/com/example/mvvm_hilt_practice/ui/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_hilt_practice/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.mvvm_hilt_practice
+package com.example.mvvm_hilt_practice.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mvvm_hilt_practice.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/mvvm_hilt_practice/ui/tasks/TasksFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mvvm_hilt_practice.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.example.mvvm_hilt_practice.R
+
+class TasksFragment : Fragment(R.layout.fragment_tasks) {
+
+}
