443aa07f87245deebe7b1685da38b263fecab8a7
JeonK1 <kan5016@naver.com>
2021-08-13 01:52:07 +0900

    [update] dagger hilt with coroutine

---
+++ app/src/main/java/com/example/mvvm_hilt_practice/ui/ToDoApplication.kt
@@ -0,0 +1,8 @@
+package com.example.mvvm_hilt_practice.ui
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ToDoApplication: Application() {
+}
---
+++ app/src/main/java/com/example/mvvm_hilt_practice/ui/tasks/TasksViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.mvvm_hilt_practice.ui.tasks
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.example.mvvm_hilt_practice.data.TaskDao
+
+class TasksViewModel @ViewModelInject constructor(
+    private val taskDao: TaskDao
+): ViewModel() {
+
+}
